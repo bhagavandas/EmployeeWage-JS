1070ee70490de83f068eed8d265e92909a2527fd
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-09-22 07:49:26 +0530

    Added: Calculated wages for a month till max days or hours reached

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,6 +4,9 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 10;
+
 
 function getWorkingHours(empCheck){
 
@@ -18,13 +21,15 @@ switch(empCheck) {
                 return 0;
 }
 }
-const NUM_OF_WORKING_DAYS = 2;
-let empHrs = 0;
-for ( let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+        totalWorkingDays++;
         let empCheck = Math.floor(Math.random() * 10) % 3;
-        empHrs += getWorkingHours(empCheck);
+        totalEmpHrs += getWorkingHours(empCheck);
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs : " + empHrs +  "  Emp Wage : " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5 - Total Days: " + totalWorkingDays + "   Total Emp Hrs: " + totalEmpHrs +  "    Emp Wage: " + empWage);
 
