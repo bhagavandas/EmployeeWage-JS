2ec8c634aad7074853122358c6e0bfc1e580b268
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-09-26 14:36:59 +0530

    Added: Object operations using array functions

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -51,3 +51,23 @@ while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAY
 }
         console.log("UC 10 Showing Daily Hours Worked and Wage Earned: " + empDailyHrsAndWageArr);
 
+
+//UC11 Object Functions along with Arrow Functions
+let totalWages = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWages > 0).reduce((totalWages, dailyHrsAndWage) => totalWages += dailyHrsAndWage.dailyWages, 0);
+let totalHours = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWages > 0).reduce((totalHours, dailyHrsAndWage) => totalHours += dailyHrsAndWage.dailyWages, 0);
+        console.log("UC 11A Total Hours: " + totalHours + "  Total Wages: " + totalWages);
+
+        process.stdout.write("UC 11B Logging Full Work Days")
+        empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 8)
+                        .forEach(dailyHrsAndWage => ProcessingInstruction.stdout.write(dailyHrsAndWage.toString()));
+
+let partWorkingDayStrArr = empDailyHrsAndWageArr
+                        .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 4)
+                        .map(dailyHrsAndWage => dailyHrsAndWage.toString());
+        console.log("\nUC 11C PartWorkingDayStrings: " + partWorkingDayStrArr);
+
+
+ let nonWorkingDayNums = empDailyHrsAndWageArr
+                        .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 0)
+                        .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
+        console.log("\nUC 11D NonWorkingDayNums: " + nonWorkingDayNums);
